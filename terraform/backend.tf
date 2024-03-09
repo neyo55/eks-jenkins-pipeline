de35131b23f9d2c1ca008aa3f90a83a37a3b7aca
adeniyi rufai <kbneyo55@gmail.com>
2024-03-09 07:47:45 +0100

    uploads

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@
 # It sets up a remote backend that stores the state file in an S3 bucket.
 terraform {
   backend "s3" {
-    bucket = "neyo-capstone-remote-backend"
+    bucket = "neyo-capstone-remote-backend-3"
     region = "eu-west-3"
     key = "eks/terraform.tfstate"
   }
